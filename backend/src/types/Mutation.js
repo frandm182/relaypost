29a323c4a615a127ede375d63f9be163df3418c7
Francisco Diaz <franciscojavier.diaz@i3television.es>
2017-12-11 14:23:00 +0100

    Adding mutation with relay

--- backend/src/types/Mutation.js
+++ backend/src/types/Mutation.js
@@ -1,32 +1,43 @@
-const { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLID } = require("graphql");
+const { 
+    GraphQLObjectType, 
+    GraphQLNonNull, 
+    GraphQLString, 
+    GraphQLID 
+} = require("graphql");
 
-const Post = require("./Post");
+const { mutationWithClientMutationId } = require("graphql-relay");
+const { Post } = require("./Post");
+
 const PostModel = require("../model/Post");
+
+const CreatePostMutation = mutationWithClientMutationId({
+    name: "CreatePost",
+    inputFields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) }
+    },
+    outputFields: {
+        post: {
+            type: Post
+        }
+    },
+    mutateAndGetPayload: args => {
+        return new Promise((resolve, reject) => {
+            PostModel.createPost({
+                title: args.title,
+                content: args.content
+            })
+            .then(post => resolve({ post }))
+            .catch(reject);
+        });
+    }
+});
 
 const Mutation = new GraphQLObjectType({
     name: "Mutation",
     description: "Mutation interface of the blog",
     fields: {
-        createPost: {
-            type: Post,
-            description: "Mutation to insert a single post",
-            args: {
-                title: { 
-                    type: GraphQLString,
-                    description: "Title of the post"                
-                },
-                content: { 
-                    type: GraphQLString,
-                    description: "Content of the post" 
-                }
-            },
-            resolve: (_, args) => {
-                return PostModel.createPost({
-                    title: args.title,
-                    content: args.content
-                });
-            }
-        }
+        createPost: CreatePostMutation
     }
 });
 
